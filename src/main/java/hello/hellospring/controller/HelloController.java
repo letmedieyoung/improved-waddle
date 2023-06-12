dbe73264ca85e7f6929aefadd3da680b1ce778ad
koyoisneverdie <lunacysky92@gmail.com>
2023-06-12 14:46:22 +0900

    회원 관리 예제
    Member.java 생성
    MemberRepository.java 생성
    MemberService.java 생성
    테이스케이스 작성

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,5 +19,30 @@ public class HelloController {
     public String hellowMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-spring")
+    @ResponseBody //http body 영역에 내용을 넣어주겠다. html 코드 형식이 아니라 데이터 노출
+    public String helloString(@RequestParam("name") String name){
+        return "hello "+ name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
--- src/test/java/HelloSpringApplicationTests.java
+++ src/test/java/HelloSpringApplicationTests.java
@@ -1,5 +1,3 @@
-package hello.hellospring;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
